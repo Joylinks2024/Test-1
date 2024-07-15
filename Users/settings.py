e68a7ea1724c903763a1d78b8592bde88e8165ae
Diyorbek-2022 <diyorbek5896@gmail.com>
2024-07-16 04:38:45 +0500

    first

--- Users/settings.py
+++ Users/settings.py
@@ -45,13 +45,18 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    # whitenoise #
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    ##############
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
 
 ROOT_URLCONF = 'Users.urls'
 

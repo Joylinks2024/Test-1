e573983d8f5e820c8b65b408953c804d5964abab
Diyorbek-2022 <diyorbek5896@gmail.com>
2024-07-16 04:29:59 +0500

    first

--- Users/settings.py
+++ Users/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d4e!2yp6*c)at**u@3mb#4qh_8p1_(-#bvvv-$e&ra#^o=vnmt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 

6173f1acb7d038a8a7a90f06adea85d51e89b30b
Diyorbek-2022 <diyorbek5896@gmail.com>
2024-07-16 18:12:34 +0500

    first1

--- Users/settings.py
+++ Users/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # MEDIA_ROOT = BASE_DIR / MEDIA_URL
 # STATIC_ROOT = BASE_DIR / 'staticfiles'
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATICFILES_DIRS = [BASE_DIR / 'static/']
+# STATICFILES_DIRS = [BASE_DIR / 'static/']
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
